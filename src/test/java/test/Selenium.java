942d0eb1549cf6cd43b6b2739f377552c8da4ed3
erdogan boyraz <boyraz9898@gmail.com>
2022-12-19 20:31:57 +0300

    19.12.2022

--- src/test/java/test/Selenium.java
+++ src/test/java/test/Selenium.java
@@ -8,6 +8,11 @@ public class Selenium {
     public static void main(String[] args) {
         System.setProperty("web driver.chrome.driver","C:\\Users\\casper\\IdeaProjects\\Selenium\\drivers\\chromedriver.exe");
         WebDriver driver=new ChromeDriver();
+          /*
+            En temel haliyle otomasyon yapmak için  Class'ımıza otaomasyon için gerekli olan
+        webdriver'in yerini göstermemiz gerekir. Bunun için java kütüphanesinden System.setProperty()
+        methodunun içine ilk olarak driver'i yazarız. İkinci olarak driver'in fiziki yolunu kopyalarız
+         */
         driver.get("https://www.amazon.com.tr");
 
     }
